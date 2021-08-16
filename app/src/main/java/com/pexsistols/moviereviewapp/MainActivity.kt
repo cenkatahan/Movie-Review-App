d193188fa3a6bf22bf29f93ff31e1f991915f60c
Cenk Atahan Özbek <atahanozzbek@gmail.com>
2021-08-16 10:58:28 +0300

    project started

---
+++ app/src/main/java/com/pexsistols/moviereviewapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pexsistols.moviereviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //comment
+    }
+}
