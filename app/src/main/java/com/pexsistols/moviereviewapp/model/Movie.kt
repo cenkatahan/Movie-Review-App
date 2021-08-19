8f73b6af5dba3e5972df25409dac3fbc333a1b6e
Cenk Atahan Özbek <atahanozzbek@gmail.com>
2021-08-19 13:05:55 +0300

    model updated

--- app/src/main/java/com/pexsistols/moviereviewapp/model/Movie.kt
+++ app/src/main/java/com/pexsistols/moviereviewapp/model/Movie.kt
@@ -1,3 +1,8 @@
 package com.pexsistols.moviereviewapp.model
 
-data class Movie(val name : String?)
+data class Movie(
+    val name : String?,
+    val year: String?,
+    val length : String?,
+    val posterUrl : String?,
+    val review : String?)
