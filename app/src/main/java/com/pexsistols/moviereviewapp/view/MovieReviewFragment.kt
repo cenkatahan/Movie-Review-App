2cc7f6af4f730ce370c7a6b0bbdf7567de546a9d
Cenk Atahan Özbek <atahanozzbek@gmail.com>
2021-08-27 22:32:35 +0300

    movie review viewmodel

--- app/src/main/java/com/pexsistols/moviereviewapp/view/MovieReviewFragment.kt
+++ app/src/main/java/com/pexsistols/moviereviewapp/view/MovieReviewFragment.kt
@@ -5,9 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.pexsistols.moviereviewapp.R
+import com.pexsistols.moviereviewapp.viewmodel.MovieFeedViewModel
+import com.pexsistols.moviereviewapp.viewmodel.MovieReviewViewModel
 
 class MovieReviewFragment : Fragment() {
+
+    private lateinit var title : TextView
+    private lateinit var director : TextView
+    private lateinit var year : TextView
+    private lateinit var length : TextView
+    private lateinit var poster : ImageView
+    private lateinit var genres : TextView
+    private lateinit var review : TextView
+    private lateinit var db : FirebaseFirestore
+    private lateinit var movieReviewViewModel: MovieReviewViewModel
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +44,10 @@ class MovieReviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        db = Firebase.firestore
+        movieReviewViewModel = ViewModelProviders.of(this).get(MovieReviewViewModel::class.java)
+
+
     }
 }
