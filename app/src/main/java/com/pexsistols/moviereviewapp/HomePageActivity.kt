6b417c345816348fcb6b4f7e2325c2057f654046
Cenk Atahan Özbek <atahanozzbek@gmail.com>
2021-09-06 17:34:23 +0300

    splash screen added

---
+++ app/src/main/java/com/pexsistols/moviereviewapp/HomePageActivity.kt
@@ -0,0 +1,18 @@
+package com.pexsistols.moviereviewapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+
+
+    }
+
+}
--- app/src/main/java/com/pexsistols/moviereviewapp/MainActivity.kt
+++ app/src/main/java/com/pexsistols/moviereviewapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.pexsistols.moviereviewapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Timer().schedule(timerTask {
+            changeScreen()
+        }, 1500)
     }
+
+
+    private fun changeScreen(){
+        val intent = Intent(this, HomePageActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
